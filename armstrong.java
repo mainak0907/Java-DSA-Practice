0f82b538b5d256d1c08f5ea3076115c1c0bb7062
unknown <chatterjee.mainak15@gmail.com>
2022-02-13 10:59:37 +0530

    Modified

--- armstrong.java
+++ armstrong.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class armstrong {
      public static void main(String[] args) { 
          try (Scanner input = new Scanner(System.in)) {
@@ -6,10 +7,15 @@ public class armstrong {
             System.out.println("Enter the number for checking");
              int n=input.nextInt();
              int sum =0;
+             int digit=0;
+             int temp=n;
              int n1=n; //Making a copy of the original number 
+             while(temp!=0){
+                 temp=temp/10;
+                 digit++;}
              while(n1!=0){
                  int rem=n1%10;
-                 sum += rem*rem*rem;
+                 sum += (Math.pow(rem,digit));
                  n1=n1/10;
                 }
                 if(sum==n){
